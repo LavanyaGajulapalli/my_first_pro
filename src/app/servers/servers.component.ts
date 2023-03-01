ea5eca9f1c8f988aa7ce688eb2324a6c84887892
LavanyaGajulapalli <lavanyagajulapalli123@gmail.com>
2023-03-01 15:33:07 +0530

    fdjhg

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  templateUrl: './servers.component.html',
+  template: `
+    <app-server></app-server>
+    <app-server></app-server>`,
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent {
+  
+// constructor{}
+// ngOnInit(){
 
+// }
 }
